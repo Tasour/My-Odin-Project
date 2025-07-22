cb6b591e429d6cf46d536c88f959245a88adf73a
Tasour <36992573+Tasour@users.noreply.github.com>
2025-07-22 22:57:24 +0200

    Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid

--- Foundations/project-Etch-a-Sketch/script.js
+++ Foundations/project-Etch-a-Sketch/script.js
@@ -1,17 +1,43 @@
 //Create 16x16 div table
-let container = document.getElementById("container")
 
-for (let index = 0; index < 16; index++) {
-    let container_div = document.createElement("div")
+function draw_table(nb_squares_per_side) {
+    let container = document.getElementById("container")
 
-    for (let index = 0; index < 16; index++) {
-        let square = document.createElement("div")
-        square.classList.add('square')
+    for (let index = 0; index < nb_squares_per_side; index++) {
+        let container_div = document.createElement("div")
 
-        square.addEventListener("mouseover", (event) => {square.classList.add("filled")})
+        for (let index = 0; index < nb_squares_per_side; index++) {
+            let square = document.createElement("div")
+            square.classList.add('square')
 
-        container_div.appendChild(square)
+            square.addEventListener("mouseover", (event) => {square.classList.add("filled")})
+
+            container_div.appendChild(square)
+        }
+
+        container.appendChild(container_div)
+    }   
+}
+
+function getUserSquarePerSide() {
+    let container = document.getElementById("container");
+    if (container.children.length > 0) {
+        container.innerHTML = "";
     }
 
-    container.appendChild(container_div)
+    let square_per_side;
+    while (true) {
+        let input = prompt("Number of squares per side ? [min: 2 to max: 99]");
+        square_per_side = parseInt(input, 10);
+        if (!isNaN(square_per_side) && square_per_side >= 2 && square_per_side <= 99) {
+            break;
+        } else {
+            alert("Please enter an integer between 2 and 99.");
+        }
+    }
+
+    draw_table(square_per_side);
 }
+
+
+document.getElementById("btn-square-size").addEventListener('click',getUserSquarePerSide)
