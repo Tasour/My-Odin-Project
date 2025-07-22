c2f2c26f7a02f1a01031b06859ebfbb32783ea38
Tasour <36992573+Tasour@users.noreply.github.com>
2025-07-22 22:38:17 +0200

    Fill background-color of squares if mouse hovers it

--- Foundations/project-Etch-a-Sketch/script.js
+++ Foundations/project-Etch-a-Sketch/script.js
@@ -1,7 +1,4 @@
-/*
-
-*/
-
+//Create 16x16 div table
 let container = document.getElementById("container")
 
 for (let index = 0; index < 16; index++) {
@@ -10,10 +7,11 @@ for (let index = 0; index < 16; index++) {
     for (let index = 0; index < 16; index++) {
         let square = document.createElement("div")
         square.classList.add('square')
+
+        square.addEventListener("mouseover", (event) => {square.classList.add("filled")})
+
         container_div.appendChild(square)
     }
 
     container.appendChild(container_div)
-
-    
 }
