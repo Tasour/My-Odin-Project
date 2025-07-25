73428225ef7acd1412a28dae70e20e5c1ad1ca5d
Tasour <36992573+Tasour@users.noreply.github.com>
2025-07-25 09:56:11 +0200

    done project calc 3.

--- Foundations/project-calulatrice-final/script.js
+++ Foundations/project-calulatrice-final/script.js
@@ -14,6 +14,21 @@ function divide(a, b) {
     return a / b
 }
 
+function operate(number_a, operator, number_b) {
+    switch (operator) {
+        case "+":
+            return add(number_a, number_b);
+        case "-":
+            return substract(number_a, number_b);
+        case "*":
+            return multiply(number_a, number_b);
+        case "/":
+            return divide(number_a, number_b);
+        default:
+            break;
+    }
+}
+
 let number_a;
 let operator;
 let number_b
