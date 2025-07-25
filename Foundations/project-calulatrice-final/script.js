88ffdfa05059a5c4b3b4de91f2bbcd925fa51a16
Tasour <36992573+Tasour@users.noreply.github.com>
2025-07-25 10:16:49 +0200

    done project calc 4
    Create a basic HTML calculator with buttons for each digit and operator

--- Foundations/project-calulatrice-final/script.js
+++ Foundations/project-calulatrice-final/script.js
@@ -24,6 +24,8 @@ function operate(number_a, operator, number_b) {
             return multiply(number_a, number_b);
         case "/":
             return divide(number_a, number_b);
+        case "%":
+            return divide(number_a, number_b);
         default:
             break;
     }
@@ -32,3 +34,22 @@ function operate(number_a, operator, number_b) {
 let number_a;
 let operator;
 let number_b
+
+let keyboard_div = document.getElementById("keyboard");
+
+for (let index = 0; index < 10; index++) {
+    let key = document.createElement('div'); // Create element
+    key.innerHTML = index
+    key.classList.add('key'); // Add class
+    keyboard_div.appendChild(key); // Append to container
+}
+
+operations_array = ["+", "-", "x", "/", "=", "Clear"]
+for (let index = 0; index < operations_array.length; index++) {
+    let key = document.createElement('div'); // Create element
+    let op = operations_array[index];
+    key.innerHTML = op
+    key.classList.add('operation_key'); // Add class
+    keyboard_div.appendChild(key); // Append to container
+}
+
